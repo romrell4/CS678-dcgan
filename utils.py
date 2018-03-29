2af8fed176ae7eb493d838ca484c57d1be7f5cd9
Ben Smith <besmith88@gmail.com>
2018-03-28 23:13:52 -0600

    made visualize function compatible with new config parameter names - dbname

--- utils.py
+++ utils.py
@@ -94,7 +94,7 @@ def visualize(sess, dcgan, config, option):
             for kdx, z in enumerate(z_sample):
                 z[idx] = values[kdx]
 
-            if config.dataset == "mnist":
+            if config.dbname == "mnist":
                 y = np.random.choice(10, config.batch_size)
                 y_one_hot = np.zeros((config.batch_size, 10))
                 y_one_hot[np.arange(config.batch_size), y] = 1
@@ -114,7 +114,7 @@ def visualize(sess, dcgan, config, option):
             for kdx, z in enumerate(z_sample):
                 z[idx] = values[kdx]
 
-            if config.dataset == "mnist":
+            if config.dbname == "mnist":
                 y = np.random.choice(10, config.batch_size)
                 y_one_hot = np.zeros((config.batch_size, 10))
                 y_one_hot[np.arange(config.batch_size), y] = 1
